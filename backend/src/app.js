4845bd0231d3f410b239ab4aae5d7f6f8db0a72e
chitranjan05 <ranjan888288@gmail.com>
2026-03-29 11:48:42 +0530

    deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,17 @@ const analyzeRoutes = require("./routes/analyze.routes");
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://commit-dna-lkc9.vercel.app', 
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", analyzeRoutes);
 
 app.get("/", (req, res) => {
   res.send("Commit DNA Backend Running 🚀");
+});
+
+app.get("/api/burnout", (req, res) => {
+  res.send("burnout page");
 });
 
 module.exports = app;
